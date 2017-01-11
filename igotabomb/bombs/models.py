70d4a98f89c1a40e77f80b069111f8a43dfd1aa9
Michael <michael.loukeris@icloud.com>
2017-01-11 16:30:05 +0200

    Create the model of bomb

--- igotabomb/bombs/models.py
+++ igotabomb/bombs/models.py
@@ -1,2 +1,22 @@
 from __future__ import unicode_literals
 from django.contrib.gis.db import models
+from django.utils.functional import cached_property
+
+class Bomb(models.Model):
+    name = models.CharField(max_length=50)
+    location = models.PointField()
+    duration_starting = models.DateTimeField()
+    duration_ending = models.DateTimeField()
+    brange = models.IntegerField()
+    image = models.ImageField(upload_to="bombs/", null=True, blank=True)
+    user_change_location = models.BooleanField(default=False)
+    change_location = models.BooleanField(default=False)
+    stealth = models.BooleanField(default=False)
+    simultaneous = models.BooleanField(default=False)
+
+    def __unicode__(self):
+        return self.name
+
+    @cached_property
+    def duration(self):
+        return self.duration_ending - self.duration_starting
