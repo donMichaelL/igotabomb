da3dfa1e0d690967ae0cd5e436e3a7a1d655ab47
Michael <michael.loukeris@icloud.com>
2017-01-11 16:11:29 +0200

    Adding PostGIS connection settings

--- igotabomb/igotabomb/settings.py
+++ igotabomb/igotabomb/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.gis',
 ]
 
 MIDDLEWARE = [
@@ -75,9 +76,13 @@ WSGI_APPLICATION = 'igotabomb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+          'ENGINE': 'django.contrib.gis.db.backends.postgis',
+          'NAME': 'postgres',
+          'USER': 'postgres',
+          'PASSWORD': 'mysecretpassword',
+          'HOST': 'localhost',
+          'PORT': 25432,
+      }
 }
 
 
